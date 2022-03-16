81f87c98390a855ca6e50269a9cc75e56b0e8ed7
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-03-16 08:51:17 -0400

    add foriegn key between task and category models and add priority attribute to task model.

--- todoApp/api/models.py
+++ todoApp/api/models.py
@@ -3,8 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+
+    HIGH = 'HIG'
+    MEDIUM = 'MED'
+    LOW = 'LOW'
+    TASK_PRIORITY_CHOICES = [
+        (HIGH, 'بالا'),
+        (MEDIUM, 'متوسط'),
+        (LOW, 'پایین'),
+    ]
+
     title = models.CharField(max_length=200, verbose_name='عنوان')
     description = models.TextField(verbose_name='توضیحات')
+    category = models.ForeignKey('api.Category', on_delete=models.PROTECT, verbose_name='دسته بندی')
+    priority = models.CharField(max_length=3, choices=TASK_PRIORITY_CHOICES, default=MEDIUM, verbose_name='الویت')
     completed = models.BooleanField(default=False, verbose_name='تکمیل شده')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
